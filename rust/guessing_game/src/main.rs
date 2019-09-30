ddbbc984320a6b1d1ff8793a5fa9654f2b41455a
Islam El-Ashi <ielashi@gmail.com>
2019-09-30 16:02:42 +0200

    Rust guessing game

---
+++ rust/guessing_game/src/main.rs
@@ -0,0 +1,32 @@
+extern crate rand;
+use rand::Rng;
+use std::io;
+
+fn main() {
+  println!("Guess the number!");
+
+  let secret_number = rand::thread_rng().gen_range(1, 101);
+
+  loop {
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+    io::stdin()
+      .read_line(&mut guess)
+      .expect("Failed to read line.");
+
+    let guess: i32 = match guess.trim().parse() {
+      Ok(num) => num,
+      Err(_) => continue,
+    };
+
+    if guess == secret_number {
+      println!("You win!");
+      break;
+    } else if guess > secret_number {
+      println!("Lower");
+    } else {
+      println!("Higher");
+    }
+  }
+}
